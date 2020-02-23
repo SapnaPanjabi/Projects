e35a8630549b0b0dbc96203fd8d09191b663f5db
sapnas7737 <panjabisapna21@gmail.com>
2020-02-23 19:06:10 +0530

    Terminal App initial commit

---
+++ TerminalApp/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TerminalApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,ISerialCommCallback
+    {
+        SerialCom serialCom = null;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            serialCom = new SerialCom(this);
+            string[] baudrate = { "1200", "4800", "9600", "19200", "38400", "115200", "921600"};
+            cmb_baudrate.SelectedItem = "9600";
+            foreach (string bdrate in baudrate)
+            {
+                cmb_baudrate.Items.Add(bdrate);
+            }
+
+            string[] ports = serialCom.getAllPortNames();
+            foreach (string comport in ports)
+            {
+                cmb_port.Items.Add(comport);
+            }
+            cmb_port.SelectedIndex = 0;
+        }
+        
+        private void txtSendBytes_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(txtSendBytes.Text);
+            serialCom.sendBytes(bytes);
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e) {
+            if (btn_connect.Content.ToString().ToUpper().Equals("CONNECT"))
+            {
+               
+                bool status = serialCom.connect(cmb_port.Text, Int32.Parse(cmb_baudrate.Text), Parity.None, 8, StopBits.One);
+                if (status)
+                {
+                    btn_connect.Content = "Disconnect";
+                    cmb_port.IsEnabled = false;
+                    cmb_baudrate.IsEnabled = false;
+                    btn_scan.IsEnabled = false;
+                }
+                else { 
+                    MessageBox.Show("Issue in connection, other application may used the port");
+                }
+            }
+            else {
+                btn_connect.Content = "Connect";
+                cmb_port.IsEnabled = true;
+                cmb_baudrate.IsEnabled = true;
+                btn_scan.IsEnabled = true;
+                serialCom.disconnect();
+            }
+        }
+
+        private void btnScan_Click(object sender, RoutedEventArgs e) {
+            string[] ports = serialCom.getAllPortNames();
+            cmb_port.Items.Clear();
+            foreach (string comport in ports)
+            {
+                cmb_port.Items.Add(comport);
+            }
+            cmb_port.SelectedIndex = 0;
+        }
+
+        private void btnClearText_Click(object sender, RoutedEventArgs e) {
+            lbl_recieveBytes.Text = "";
+        }
+
+        public void setData(string data)
+        {
+            lbl_recieveBytes.Text += data + "\n";
+        }
+    }
+}
---
+++ TerminalApp/SerialCom.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TerminalApp
+{
+    class SerialCom
+    {
+        SerialPort port;
+        ISerialCommCallback iSerialCommCallback;
+        private delegate void SetTextDeleg(string text);
+        MainWindow context;
+
+        public SerialCom(MainWindow mContext)
+        {
+            iSerialCommCallback= (ISerialCommCallback)mContext;
+            context = mContext;
+        }
+        void serialPort_DataRecieved(object sender, SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(10);
+            try
+            {
+                string data = port.ReadExisting();
+                context.Dispatcher.Invoke(new SetTextDeleg(si_DataReceived), new object[] { data });
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+        public string[] getAllPortNames() {
+            return SerialPort.GetPortNames();
+        } 
+
+
+        private void si_DataReceived(string data)
+        {
+            if (port != null)
+            {
+                var bytes = Encoding.UTF8.GetBytes(data);
+                var hexString = BitConverter.ToString(bytes).Replace("-","");
+                iSerialCommCallback.setData(hexString);
+            }
+        }
+
+        public void sendBytes(byte[] bytes) {
+            try
+            {
+                port.Write(bytes, 0, bytes.Length);
+            }
+
+            catch (Exception ex) { }
+        }
+
+        public bool connect(string portName, int baudrate, Parity parity, int dataBits, StopBits stopBit)
+        {
+            port = new SerialPort(portName, baudrate, parity, dataBits, stopBit);
+            port.Encoding = System.Text.Encoding.UTF8;
+            port.Handshake = Handshake.None;
+            port.ReadTimeout = 10000;
+
+            if (port != null)
+            {
+                try
+                {
+                    if (!port.IsOpen)
+                    {
+                        port.Open();
+                    }
+                    port.DataReceived += new SerialDataReceivedEventHandler(serialPort_DataRecieved);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public void disconnect()
+        {
+            if (port != null && port.IsOpen)
+            {
+                port.Close();
+            }
+        }
+    }
+}
